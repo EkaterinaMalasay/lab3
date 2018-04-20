2d96587139d92688c0dc3fda71f394b3cdc56851
EkaterinaMalasay <ekaterina.malasay@gmail.com>
2018-04-20 21:42:51 +0300

    +keys

--- term.c
+++ term.c
@@ -4,39 +4,42 @@
 #include <string.h>
 #define N 1024
 
-char **environ;
-
 void main(void)
 {
-    int i = 0, k = 3;
+    int i = 0;
     char com[N] = {0};
     char com2[N] = "/bin/";
-    char* argv[2];
+    char* argv[N] = {0};
+    char *pch = " ";
     pid_t new_pid;
 
-while(k>0)
-{
-    write(1, ">>> ", 5);
-    read(0, com, N);
-    i = strcspn(com, "\n");
-    com[i] = 0;
-    strcat(com2, com);
-    argv[0] = com2;
-    argv[1] = (char*) 0;
-    new_pid = 1;
-    new_pid = fork();
-    if (new_pid == 0){
-        execv(argv[0], argv);
-        exit(0);
+    while(1)
+    {
+        write(1, ">>> ", 5);
+        read(0, com, N);
+        i = strcspn(com, "\n");
+        com[i] = 0;
+        if(strcmp(com, "exit") == 0) break;
+        strcat(com2, com);
+
+        for(i=0, pch = strtok(com2," ");pch != NULL;i++){
+            argv[i] = pch;
+            pch = strtok(NULL, " ");
+        }
+        argv[N] = (char*) 0;
+
+        new_pid = fork();
+        if (new_pid == 0){
+            execv(argv[0], argv);
+            exit(0);
+        }
+        else{
+            sleep(1);
+        }
+        memset(com, 0, N);
+        memset(com2, 0, N);
+        memset(argv, 0, N);
+        strcat(com2, "/bin/");
     }
-    else{
-        sleep(1);
-    }
-    memset(com, 0, N);
-    memset(com2, 0, N);
-    memset(argv, 0, N);
-    strcat(com2, "/bin/");
-    k--;
-}
     exit(0);
 }
